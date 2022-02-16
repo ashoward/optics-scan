db1c8f22d9898aa75f30b13b46dcd3feb5499993
Mei-Li Holmberg <mei-li.holmberg@cern.ch>
2022-02-16 14:54:35 +0100

    renamed batch file

--- Batch/batch-scan-all.tcl
+++ Batch/batch-scan-all.tcl
@@ -1,10 +1,14 @@
+
+# Script to run scans in batch mode
+# Syntax: vivado -mode batch -source batch-scan-all.tcl
+
 open_hw_manager
 connect_hw_server -allow_non_jtag
 open_hw_target {localhost:3121/xilinx_tcf/Xilinx/00000000000000}
 current_hw_device [get_hw_devices xcvu7p_0]
 refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xcvu7p_0] 0]
 
-# Program device?
+# Program device? Currently done interactively
 
 # Generate links
 source /home/meholmbe/optics-scan/GenerateLinks/gen_links_vu7p_alpha.tcl
