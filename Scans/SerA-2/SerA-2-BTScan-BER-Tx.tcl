e6b795699d0ce03e0355fd5aefd6e0b5592a0b0a
Luis Ardila <luis.ardila@kit.edu>
2022-03-30 11:18:04 +0200

    add board variable

--- Scans/SerA-2/SerA-2-BTScan-BER-Tx.tcl
+++ Scans/SerA-2/SerA-2-BTScan-BER-Tx.tcl
@@ -12,6 +12,7 @@ set folderTime [clock format $systemTime -format %Y-%m-%d-%H%M]
 # Output folder
 set folderName "/home/luis/Projects/optics-scan/results/SerA-2_BTScan_BER_Tx_$folderTime"
 # Board name
+set board "xcvu9p_0"
 set boardName "root@128.141.223.196"
 # Paths to scripts on the board
 set setEqScript "/root/optical_eval/setPre.sh"
@@ -400,7 +401,7 @@ foreach group $groups {
 
                 # Measure BER and error count
                 # Do it after the bathtub scan or one get an "incorrect" error count
-                refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xcvu9p_0] 0] 
+                refresh_hw_device -update_hw_probes false [lindex [get_hw_devices $board] 0] 
                 set_property LOGIC.MGT_ERRCNT_RESET_CTRL 1 [get_hw_sio_links $link]
                 commit_hw_sio [get_hw_sio_links $link]
                 set_property LOGIC.MGT_ERRCNT_RESET_CTRL 0 [get_hw_sio_links $link]
